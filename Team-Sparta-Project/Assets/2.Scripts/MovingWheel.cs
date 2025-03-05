b4f452fba0be3074ef594f50a5077c4ba62f62da
Lee Ui Hwan <luh5063014@naver.com>
2025-03-05 18:50:29 +0900

    Chore: 바퀴 애니메이션 추가

---
+++ Team-Sparta-Project/Assets/2.Scripts/MovingWheel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWheel : MonoBehaviour
+{
+    // 바퀴 회전 애니메이션 스크립트
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private void Update()
+    {
+        this.transform.Rotate(new Vector3(0f, 0f, moveSpeed) * Time.deltaTime);
+    }
+}
