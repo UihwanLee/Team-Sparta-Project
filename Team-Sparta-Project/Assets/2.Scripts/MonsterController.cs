040df22b9f45205dfd14d147e838761f63b13184
Lee Ui Hwan <luh5063014@naver.com>
2025-03-06 17:09:11 +0900

    Chore: 3가지 경로를 지정하기 각기 다른 경로로 몬스터 배치
    몬스터 간의 충돌 설정도 필요

--- Team-Sparta-Project/Assets/2.Scripts/MonsterController.cs
+++ Team-Sparta-Project/Assets/2.Scripts/MonsterController.cs
@@ -11,14 +11,37 @@ public class MonsterController : MonoBehaviour
     [SerializeField]
     private Vector3 moveDir;
 
+    private int spawnPath;      // 주어진 path 경로
+
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
         // 소환될 시 기본적으로 옆으로 이동
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    public void SetPath(int myPath, Collider2D myCollider, Collider2D[] ignorePath1, Collider2D[] ignorePath2)
+    {
+        // 주어진 경로 지정
+        spawnPath = myPath;
+
+        // 정한 path 외 다른 path 충돌처리 무시
+        IgnoreCollisionWithPaths(myCollider, ignorePath1);
+        IgnoreCollisionWithPaths(myCollider, ignorePath2);
+    }
+
+    private void IgnoreCollisionWithPaths(Collider2D myCollider, Collider2D[] ignorePath)
+    {
+        foreach (Collider2D pathCollider in ignorePath)
+        {
+            if (pathCollider != null && myCollider != null)
+            {
+                Physics2D.IgnoreCollision(myCollider, pathCollider);
+            }
+        }
     }
 }
--- Team-Sparta-Project/Assets/2.Scripts/MonsterSpawner.cs
+++ Team-Sparta-Project/Assets/2.Scripts/MonsterSpawner.cs
@@ -13,11 +13,36 @@ public class MonsterSpawner : MonoBehaviour
 
     private float curTime;
 
+    // 3가지 경로 Collider
+    private List<Collider2D[]> pathColliders = new List<Collider2D[]>();
+
     // Start is called before the first frame update
     void Start()
     {
         curTime = spawnTime;
+        InitPathColliders();
         StartCoroutine(SpawnMonsters());
+    }
+
+    private void InitPathColliders()
+    {
+        GameObject[] path1Objects = GameObject.FindGameObjectsWithTag("Path_1");
+        GameObject[] path2Objects = GameObject.FindGameObjectsWithTag("Path_2");
+        GameObject[] path3Objects = GameObject.FindGameObjectsWithTag("Path_3");
+
+        pathColliders.Add(GetColliders(path1Objects));  
+        pathColliders.Add(GetColliders(path2Objects));  
+        pathColliders.Add(GetColliders(path3Objects));  
+    }
+
+    private Collider2D[] GetColliders(GameObject[] pathObjects)
+    {
+        Collider2D[] colliders = new Collider2D[pathObjects.Length];
+        for (int i = 0; i < pathObjects.Length; i++)
+        {
+            colliders[i] = pathObjects[i].GetComponent<Collider2D>();
+        }
+        return colliders;
     }
 
     IEnumerator SpawnMonsters()
@@ -27,7 +52,47 @@ public class MonsterSpawner : MonoBehaviour
             // 특정 주기 동안 몬스터 소환
             yield return new WaitForSeconds(spawnTime);
 
-            Instantiate(monsterPrefab, this.transform.position, Quaternion.identity);   // 몬스터 소환
+            var monster = Instantiate(monsterPrefab, this.transform.position, Quaternion.identity);   // 몬스터 소환
+
+            if(monster != null)
+            {
+                SetMonsterPath(monster);
+            }
         }
+    }
+
+    private void SetMonsterPath(GameObject monster)
+    {
+        // 경로 3개 중 1개를 랜덤으로 선택
+        int randomPathIndex = Random.Range(0, 3);  // 0, 1, 2 중 하나를 선택 (Path1, Path2, Path3)
+
+        Collider2D myCollider = monster.GetComponent<Collider2D>();
+        Collider2D[] collider1 = null;
+        Collider2D[] collider2 = null;
+
+        Debug.Log("선택경로: " + randomPathIndex);
+
+        // 경로 선택
+        switch (randomPathIndex)
+        {
+            case 0:  // Path1 선택
+                collider1 = pathColliders[0];
+                collider2 = pathColliders[1]; // 나머지 2개 경로에서 선택
+                break;
+
+            case 1:  // Path2 선택
+                collider1 = pathColliders[1];
+                collider2 = pathColliders[2]; // 나머지 2개 경로에서 선택
+                break;
+
+            case 2:  // Path3 선택
+                collider1 = pathColliders[2];
+                collider2 = pathColliders[0]; // 나머지 2개 경로에서 선택
+                break;
+        }
+
+        // 몬스터에게 경로 전달
+        MonsterController monsterController = monster.GetComponent<MonsterController>();
+        monsterController.SetPath(randomPathIndex, myCollider, collider1, collider2);
     }
 }
