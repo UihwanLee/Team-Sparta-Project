e7f6eaef0957466625e3130e9a35f6aeff52314b
Lee Ui Hwan <luh5063014@naver.com>
2025-03-07 17:21:51 +0900

    Chore: 몬스터가 2마리 있을 때 움직임 구현 성공
    몬스터 3마리 이상 있을 때 움직임 구현 필요

--- Team-Sparta-Project/Assets/2.Scripts/MonsterController.cs
+++ Team-Sparta-Project/Assets/2.Scripts/MonsterController.cs
@@ -11,7 +11,7 @@ public class MonsterController : MonoBehaviour
     [SerializeField] private int id;                                // 몬스터 id
     [SerializeField] private string title;                          // 몬스터 타이틀
     [SerializeField] private int level;                             // 몬스터 레벨
-    [SerializeField] private int spawnPath;                         // 몬스터 경로
+    [SerializeField] private string layer;                           // 몬스터 경로
     [SerializeField] private float speed;                           // 몬스터 스피드
     [SerializeField] private Vector3 moveDir;                       // 몬스터 이동 방향
     [SerializeField] private int hp;                                // 몬스터 체력.
@@ -20,97 +20,224 @@ public class MonsterController : MonoBehaviour
 
     [Header("Monster BT")]
     [SerializeField] private Selector root;
+    [SerializeField] private bool isMonsterAround;
+
+    [Header("Monster Physics")]
+    private Rigidbody2D rb;
+    [SerializeField] public float jumpForce = 5f;
+    [SerializeField] private bool isJumping = false;
+    [SerializeField] Collider2D myCollider;
+    Collider2D[] hitColliders;
+
+    public float climbHeight = 4.0f;      // 올라갈 높이
+    public float slideSpeed = 1f;       // 미끄러지는 속도
+    [SerializeField] private bool isClimbing = false;
+    [SerializeField] private bool isSliding = false;
 
     private void Start()
     {
-        InitBehaviorTree();
+        rb = GetComponent<Rigidbody2D>();
+        isMonsterAround = false;
+        //InitBehaviorTree();
     }
 
-    private void InitBehaviorTree()
-    {
-        root = new Selector();
-        Sequence actionSequence = new Sequence();
-        Sequence jumpSequence = new Sequence();
-        Sequence attackSequence = new Sequence();
-        Sequence chaseSequence = new Sequence();
+    //private void InitBehaviorTree()
+    //{
+    //    root = new Selector();
+    //    Sequence actionSequence = new Sequence();
+    //    Sequence jumpSequence = new Sequence();
+    //    Sequence attackSequence = new Sequence();
+    //    Sequence chaseSequence = new Sequence();
 
-        Condition isPlayerAround = new Condition(IsPlayerAround);
-        Condition isMonsterAround = new Condition(IsMonsterAround);
+    //    Condition isPlayerAround = new Condition(IsPlayerAround);
+    //    Condition isMonsterAround = new Condition(IsMonsterAround);
 
-        Action jumpAction = new Action(Jumping);
-        Action attackAction = new Action(Attack);
-        Action chaseAction = new Action(Chase);
+    //    Action jumpAction = new Action(TryJumping);
+    //    Action attackAction = new Action(Attack);
+    //    Action chaseAction = new Action(Chase);
 
-        root.AddChild(actionSequence);
-        root.AddChild(chaseSequence);
+    //    root.AddChild(actionSequence);
+    //    root.AddChild(chaseSequence);
 
-        actionSequence.AddChild(jumpSequence);
-        actionSequence.AddChild(attackSequence);
-        chaseSequence.AddChild(chaseAction);
+    //    actionSequence.AddChild(jumpSequence);
+    //    actionSequence.AddChild(attackSequence);
+    //    chaseSequence.AddChild(chaseAction);
 
-        jumpSequence.AddChild(isMonsterAround);
-        jumpSequence.AddChild(jumpAction);
-        attackSequence.AddChild(isPlayerAround);
-        attackSequence.AddChild(attackAction);
+    //    jumpSequence.AddChild(isMonsterAround);
+    //    jumpSequence.AddChild(jumpAction);
+    //    attackSequence.AddChild(isPlayerAround);
+    //    attackSequence.AddChild(attackAction);
 
-        root.Run();
-    }
+    //    root.Run();
+    //}
 
     private void Update()
     {
         // BehaviorTree 동작
-        root.Run();
-    }
-
-    private NodeState Jumping()
-    {
-        Debug.Log("Jummping!");
-        return NodeState.SUCCESS;
-    }
-
-    private NodeState Attack()
-    {
-        Debug.Log("Attack Player!");
-        return NodeState.SUCCESS;
-    }
-
-    private NodeState Chase()
-    {
-        Debug.Log("Chase Player!");
-        transform.position += moveDir * speed * Time.deltaTime;
-        return NodeState.SUCCESS;
-    }
-
-    private bool IsPlayerAround()
-    {
-        return false;
-    }
-    
-    private bool IsMonsterAround()
-    {
-        return false;
-    }
-
-    public void SetPath(int myPath, Collider2D myCollider, Collider2D[] ignorePath1, Collider2D[] ignorePath2)
-    {
-        // 주어진 경로 지정
-        spawnPath = myPath;
-
-        // 정한 path 외 다른 path 충돌처리 무시
-        IgnoreCollisionWithPaths(myCollider, ignorePath1);
-        IgnoreCollisionWithPaths(myCollider, ignorePath2);
-    }
-
-    private void IgnoreCollisionWithPaths(Collider2D myCollider, Collider2D[] ignorePath)
-    {
-        foreach (Collider2D pathCollider in ignorePath)
+        if(isSliding)
         {
-            if (pathCollider != null && myCollider != null)
+            SlideDown();
+        }
+        else if (isClimbing)
+        {
+            if(isJumping==false) ClimbUp();
+        }
+        else
+        {
+            Moving();
+        }
+        //root.Run();
+    }
+
+    void Moving()
+    {
+        //if(isSliding) { Debug.Log("위에 누가 있음!"); }
+        transform.position += moveDir * speed * Time.deltaTime;
+        //Vector2 moveDirection = Vector2.left;  // 왼쪽으로 이동
+        //rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y);
+    }
+
+    // 미끄러지는 동작
+    void SlideDown()
+    {
+        // 충돌 시 뒤에 있는 몬스터가 밀려날 수 있도록 속도를 설정할 수 있음
+        //transform.position += moveDir * speed * -0.1f * Time.deltaTime;
+
+        StartCoroutine(SlideDownCoroutine());
+
+        //Vector2 pushDirection = Vector2.right;
+        //rb.AddForce(pushDirection * speed, ForceMode2D.Force);
+    }
+
+    private IEnumerator SlideDownCoroutine()
+    {
+        rb.velocity = new Vector2(1.0f, rb.velocity.y);
+
+        yield return new WaitForSeconds(1.0f); // 밀리는 시간
+
+        isSliding = false;
+    }
+
+    // 올라가는 동작
+    void ClimbUp()
+    {
+        StartCoroutine(ClimbUpCoroutine());
+    }
+
+    private IEnumerator ClimbUpCoroutine()
+    {
+        isJumping = true;
+
+        // 점프할 동안 몬스터 간 충돌 무시
+        hitColliders = Physics2D.OverlapCircleAll(transform.position, 1.5f, LayerMask.NameToLayer(layer));
+
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+        yield return new WaitForSeconds(0.5f); // 점프 유지 시간
+
+        isClimbing = false;
+
+        yield return new WaitForSeconds(5.0f); // 쿨타임
+
+        isJumping = false; // 점프가 끝나면 다시 점프 가능
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer(layer))
+        {
+            int hitCondition = GetHitPosition(collision);
+
+            switch (hitCondition)
             {
-                Physics2D.IgnoreCollision(myCollider, pathCollider);
+                case 0:
+                    isSliding = true;
+                    isClimbing = false;
+                    break;
+                case 1:
+                    if (isJumping == false && isSliding == false)
+                    {
+                        isClimbing = true;
+                    }
+                    isSliding = false;
+                    break;
+                case -1:
+                    break;
+                default:
+                    isSliding = false;
+                    isClimbing = false;
+                    break;
             }
         }
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer(layer))
+        {
+            //Debug.Log("벗어남");
+
+            //if (isSliding) isSliding = false; 
+        }
+    }
+
+    private int GetHitPosition(Collision2D collision)
+    {
+        float otherMonsterX = collision.transform.position.x;
+        float otherMonsterY = collision.transform.position.y;
+        float myX = transform.position.x;
+        float myY = transform.position.y;
+
+        //if(otherMonsterY - myY > 0) Debug.Log(otherMonsterY - myY);
+
+        if (otherMonsterY - myY > 1.0f)
+        {
+            return 0;
+        }
+        else if(myX - otherMonsterX > 0.5f)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public void SetPath(string myLayer, Collider2D _collider, Collider2D[] ignorePath1, Collider2D[] ignorePath2)
+    {
+        if (myLayer == "") Debug.LogError("There is no path!");
+
+        // 주어진 경로 지정
+        layer = myLayer;
+
+        // 정한 path 외 다른 path 충돌처리 무시
+        IgnoreCollisionList(_collider, ignorePath1);
+        IgnoreCollisionList(_collider, ignorePath2);
+    }
+
+    private void IgnoreCollisionList(Collider2D _collider, Collider2D[] ignoreColliders)
+    {
+        foreach (Collider2D collider in ignoreColliders)
+        {
+            if (collider != null && _collider != null)
+            {
+                Physics2D.IgnoreCollision(collider, _collider);
+            }
+        }
+    }
+
+    private void ResetCollisionList(Collider2D _collider, Collider2D[] ignoreColliders)
+    {
+        foreach (Collider2D collider in ignoreColliders)
+        {
+            if (collider != null && _collider != null)
+            {
+                Physics2D.IgnoreCollision(_collider, collider, true);
+            }
+        }
+    }
+
     public void SetID(int _id) { id = _id; }
     public int GetID() { return id; }
     public void SetSprites(List<Sprite> _sprites)
@@ -131,4 +258,46 @@ public class MonsterController : MonoBehaviour
             render.sortingOrder += amount;
         }
     }
+
+    //private NodeState TryJumping()
+    //{
+    //    if (isJumping == false)
+    //    {
+    //        return Jumping();
+    //    }
+    //    return NodeState.FAIL;
+    //}
+
+    //private NodeState Jumping()
+    //{
+    //    //Debug.Log("Jummping!");
+
+    //    // 점프 실행
+    //    StartCoroutine(SmoothJump());
+    //    return NodeState.SUCCESS;
+    //}
+
+    //private NodeState Attack()
+    //{
+    //    Debug.Log("Attack Player!");
+    //    return NodeState.SUCCESS;
+    //}
+
+    //private NodeState Chase()
+    //{
+    //    //Debug.Log("Chase Player!");
+    //    Vector2 moveDirection = Vector2.left;  // 왼쪽으로 이동
+    //    rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y);
+    //    return NodeState.SUCCESS;
+    //}
+
+    //private bool IsPlayerAround()
+    //{
+    //    return false;
+    //}
+
+    //private bool IsMonsterAround()
+    //{
+    //    return isJumping ? true : isMonsterAround;
+    //}
 }
--- Team-Sparta-Project/Assets/2.Scripts/MonsterPathManager.cs
+++ Team-Sparta-Project/Assets/2.Scripts/MonsterPathManager.cs
@@ -2,6 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class PathTag
+{
+    public const string PATH_1 = "Path_1";
+    public const string PATH_2 = "Path_2";
+    public const string PATH_3 = "Path_3";
+}
+
+public static class PathLayer
+{
+    public const string PATH_1 = "Monster Path1";
+    public const string PATH_2 = "Monster Path2";
+    public const string PATH_3 = "Monster Path3";
+}
+
 public class MonsterPathManager : MonoBehaviour
 {
     // 몬스터 경로 관리 스크립트
@@ -43,9 +57,9 @@ public class MonsterPathManager : MonoBehaviour
 
     private void InitPathColliders()
     {
-        GameObject[] path1Objects = GameObject.FindGameObjectsWithTag("Path_1");
-        GameObject[] path2Objects = GameObject.FindGameObjectsWithTag("Path_2");
-        GameObject[] path3Objects = GameObject.FindGameObjectsWithTag("Path_3");
+        GameObject[] path1Objects = GameObject.FindGameObjectsWithTag(PathTag.PATH_1);
+        GameObject[] path2Objects = GameObject.FindGameObjectsWithTag(PathTag.PATH_2);
+        GameObject[] path3Objects = GameObject.FindGameObjectsWithTag(PathTag.PATH_3);
 
         pathColliders.Add(GetColliders(path1Objects));
         pathColliders.Add(GetColliders(path2Objects));
@@ -69,35 +83,45 @@ public class MonsterPathManager : MonoBehaviour
         // 경로 3개 중 1개를 랜덤으로 선택
         int path = Random.Range(0, 3);
 
+        string pathLayer = "";
+
         Collider2D myCollider = monster.GetComponent<Collider2D>();
         Collider2D[] collider1 = null;
         Collider2D[] collider2 = null;
 
         // 경로 선택
-        switch (path)
-        {
-            case 0:  // Path1 선택
-                collider1 = pathColliders[1];
-                collider2 = pathColliders[2];
-                monster.layer = LayerMask.NameToLayer("Monster Path1");
-                break;
+        //switch (path)
+        //{
+        //    case 0:  // Path1 선택
+        //        collider1 = pathColliders[1];
+        //        collider2 = pathColliders[2];
+        //        pathLayer = PathLayer.PATH_1;
+        //        monster.layer = LayerMask.NameToLayer(PathLayer.PATH_1);
+        //        break;
 
-            case 1:  // Path2 선택
-                collider1 = pathColliders[0];
-                collider2 = pathColliders[2];
-                monster.layer = LayerMask.NameToLayer("Monster Path2");
-                break;
+        //    case 1:  // Path2 선택
+        //        collider1 = pathColliders[0];
+        //        collider2 = pathColliders[2];
+        //        pathLayer = PathLayer.PATH_2;
+        //        monster.layer = LayerMask.NameToLayer(PathLayer.PATH_2);
+        //        break;
 
-            case 2:  // Path3 선택
-                collider1 = pathColliders[0];
-                collider2 = pathColliders[1];
-                monster.layer = LayerMask.NameToLayer("Monster Path3");
-                break;
-        }
+        //    case 2:  // Path3 선택
+        //        collider1 = pathColliders[0];
+        //        collider2 = pathColliders[1];
+        //        pathLayer = PathLayer.PATH_3;
+        //        monster.layer = LayerMask.NameToLayer(PathLayer.PATH_3);
+        //        break;
+        //}
+
+        collider1 = pathColliders[1];
+        collider2 = pathColliders[2];
+        pathLayer = PathLayer.PATH_1;
+        monster.layer = LayerMask.NameToLayer(PathLayer.PATH_1);
 
         // 몬스터에게 경로 전달
         MonsterController monsterController = monster.GetComponent<MonsterController>();
-        monsterController.SetPath(path, myCollider, collider1, collider2);
+        monsterController.SetPath(pathLayer, myCollider, collider1, collider2);
         monsterController.AddOrderLayer((4-path)*10);
     }
 }
