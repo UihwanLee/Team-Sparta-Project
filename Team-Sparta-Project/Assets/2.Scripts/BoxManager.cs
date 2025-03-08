5bfcf52474cab54db0c88c9baf857a741a6dddef
Lee Ui Hwan <luh5063014@naver.com>
2025-03-08 13:44:44 +0900

    Chore: Box 오브젝트에 Box Tag 적용

--- Team-Sparta-Project/Assets/2.Scripts/BoxManager.cs
+++ Team-Sparta-Project/Assets/2.Scripts/BoxManager.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+public static class BoxTag
+{
+    public const string boxTag = "Box";
+}
+
 public class BoxManager : MonoBehaviour
 {
     // 박스 오브젝트들을 관리하는 스크립트
@@ -26,6 +31,9 @@ public class BoxManager : MonoBehaviour
 
                 if(boxObj.gameObject.GetComponent<Box>() == null)
                 {
+                    // box tag 변경
+                    boxObj.gameObject.tag = BoxTag.boxTag;
+
                     // box에 스크립트 추가
                     Box box = boxObj.gameObject.AddComponent<Box>();
                     box.SetBoxSetting();
