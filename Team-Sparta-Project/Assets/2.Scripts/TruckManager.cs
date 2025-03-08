1642cac3766a82e8c15a85fc7171dcbb36554b36
Lee Ui Hwan <luh5063014@naver.com>
2025-03-08 15:30:52 +0900

    Chore: Truck 내 오브젝트를 관리할 TruckManager 추가

---
+++ Team-Sparta-Project/Assets/2.Scripts/TruckManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public static class BoxData
+{
+    public const string boxTag = "Box";
+}
+
+public class TruckManager : MonoBehaviour
+{
+    // 트럭 내 오브젝트들을 관리하는 스크립트
+
+    [Header("WheelObject")]
+    [SerializeField] private List<MovingWheel> wheels = new List<MovingWheel>();
+
+    [Header("BoxObject")]
+    [SerializeField] private Transform boxTrans;
+    [SerializeField] private List<Box> boxList = new List<Box>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitBoxList();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    void InitBoxList()
+    {
+        foreach(Transform boxObj in boxTrans)
+        {
+            if(boxObj.name.Contains("Box"))
+            {       
+                if(boxObj.gameObject.GetComponent<Box>() == null)
+                {
+                    // box tag 변경
+                    boxObj.gameObject.tag = BoxData.boxTag;
+
+                    // box에 스크립트 추가
+                    Box box = boxObj.gameObject.AddComponent<Box>();
+                    box.SetBoxSetting();
+
+                    // boxList에 추가
+                    boxList.Add(box);
+                }
+            }
+        }
+
+    }
+}
