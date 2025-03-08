a8aa582759409dd09bbde0e78eddc5bf92634b4a
Lee Ui Hwan <luh5063014@naver.com>
2025-03-08 17:42:43 +0900

    Chore: Hero 스크립트 추가

---
+++ Team-Sparta-Project/Assets/2.Scripts/HeroController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    // 히어로 컨트롤러 스크립트
+
+    // 몬스터 정보
+    [Header("Hero Info")]
+    [SerializeField] private int hp;                                // 히어로 체력.
+    [SerializeField] private int damage;                            // 히어로 공격력
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void Damage()
+    {
+
+    }
+
+    private void Dead()
+    {
+
+    }
+}
